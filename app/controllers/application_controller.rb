42c6332b5fc5024aac27184723ff568811b37fc5
EC2 Default User <ec2-user@ip-172-31-7-97.ap-northeast-1.compute.internal>
2022-07-18 09:45:22 +0000

    _side編集中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(@user.id)
   end
+
+
 
 
   def after_sign_out_path_for(resource)
