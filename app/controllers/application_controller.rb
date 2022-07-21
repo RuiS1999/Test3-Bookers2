535de8774cd3118702cb43b93549e1e2ff68642d
EC2 Default User <ec2-user@ip-172-31-7-97.ap-northeast-1.compute.internal>
2022-07-21 07:04:38 +0000

    Finish ver.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
-    flash[:notice] = "Welcome! You have signed up successfully."
-    user_path(current_user.id)
-  end
-
   def after_sign_in_path_for(resource)
-    flash[:notice] = "Signed in successfully."
+    # flash[:notice] = "Signed in successfully."
     user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
-    flash[:notice] = "Signed out successfully."
+    # flash[:notice] = "Signed out successfully."
     root_path
   end
 
